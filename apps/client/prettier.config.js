5d4666214abc04ff52ab3d4ce0ae22fa0b6caf37
Полюбай Михаил <mikhail.polyubay@gazprombank.ru>
2025-03-03 22:09:20 +0300

    Инициализация проекта

---
+++ apps/client/prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    tabWidth: 2,
+    trailingComma: "es5",
+    printWidth: 100,
+    semi: true,
+};
