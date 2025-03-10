3bd350c189ad87cf717c1edcb11daacad5277e86
Полюбай Михаил <optimisist@gmail.com>
2025-03-10 21:43:42 +0300

    Обновлён lock файл монорепозитория, добавлена проверка типов перед пушем.

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    resolve: {
+        alias: {
+            "@routes": path.resolve(__dirname, "./src/app/routes"),
+            "@pages": path.resolve(__dirname, "./src/pages"),
+            "@entities": path.resolve(__dirname, "./src/entities"),
+            "@api": path.resolve(__dirname, "./src/shared/api"),
+            "@ui/*": path.resolve(__dirname, "./src/shared/ui"),
+        },
+    },
+    plugins: [react()],
 })
