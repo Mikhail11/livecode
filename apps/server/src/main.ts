126fe86a3082c4ad0f375d79db0b0d37c7516349
rizespb <rizespb@yandex.ru>
2025-03-21 17:15:28 +0300

    Dockerfile.server

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,7 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '~app';
 
-const PORT = process.env.PORT || 3000;
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 async function startApp() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +13,9 @@ async function startApp() {
   // Включаем автоматическую валидация согласно правилам, описанным в DTO
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  await app.listen(SERVER_PORT, () =>
+    console.log(`Server started on port ${SERVER_PORT}`),
+  );
 }
 
 startApp();
