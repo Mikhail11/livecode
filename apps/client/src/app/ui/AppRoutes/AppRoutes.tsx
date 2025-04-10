7deea6ca385a4ff6520323f4a97c8c14f0a1051c
Полюбай Михаил <optimisist@gmail.com>
2025-04-10 22:53:52 +0300

    Добавлена поле для редактирования и превью кода.

--- apps/client/src/app/ui/AppRoutes/AppRoutes.tsx
+++ apps/client/src/app/ui/AppRoutes/AppRoutes.tsx
@@ -1,5 +1,5 @@
-import { HomePage } from '@pages/Home';
-import { RoomPage } from '@pages/Room';
+import { HomePage } from '@pages/home';
+import { RoomPage } from '@pages/room';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 export const AppRoutes = () => {
